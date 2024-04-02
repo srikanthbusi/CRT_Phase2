f8d9b29df0d6b1f31b840e278b6b35ae02734e43
srikanthbusi <srikanthbusi3@gmail.com>
2024-04-02 15:26:19 +0530

    commit

--- Second_Day/Singlelinkedlist.py
+++ Second_Day/Singlelinkedlist.py
@@ -10,23 +10,36 @@ head.next = second
 second.next= third
 third.next = fourth
 def display(head):
-        
+        count = 0
         while head!=None:
             print(head.data)
             head=head.next
-def insert_atb(data,head):
+            count +=1
+        print(f"Linked list consist of {count} elements")
+def insert_atb(data,head): # a function which takes two arguments data and next nodes and display the apended nodes in the front
      newNode = Node(data)
      newNode.next = head
      head = newNode
      return head
 
-def insert_at_e(data,head):
+def insert_at_e(data,head): # a function which takes two arguments data and next nodes and display the apended nodes in the back
     newNode =Node(data)
     temp = head
     while temp.next!=None:
         temp = temp.next
     temp.next = newNode
     return head
+def mid(head):
+    l =  display(head)
+    middle = l//2
+    c1 = 0
+    temp =head
+    while temp!=None and c1<=middle:
+        c1 = c1+1
+        temp = temp.next
+    return temp.data
+    
+     
     
 display(head)
 head = insert_atb(65,head)
@@ -37,11 +50,3 @@ head = insert_at_e(65,head)
 head = insert_at_e(21,head)
 print(head)
 display(head)
-
-
-
-
-
-
-
-# write a function which takes two arguments data and next nodes and display the apended nodes in the front
